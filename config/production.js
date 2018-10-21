31071a23437e17255463e39152d0a8cf34dec9a6
Tony Spiro <tspiro@tonyspiro.com>
2018-10-21 11:46:07 -0500

    rm SMTPS_STRING

--- config/production.js
+++ config/production.js
@@ -2,6 +2,5 @@
 export default {
   COSMIC_BUCKET: process.env.COSMIC_BUCKET,
   COSMIC_READ_KEY: process.env.COSMIC_READ_KEY,
-  COSMIC_WRITE_KEY: process.env.COSMIC_WRITE_KEY,
-  SMTPS_STRING: process.env.SMTPS_STRING
+  COSMIC_WRITE_KEY: process.env.COSMIC_WRITE_KEY
 }
